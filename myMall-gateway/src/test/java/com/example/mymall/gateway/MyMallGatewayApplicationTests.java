2d1561dd2c9cb208b689617882d08d72d0bcbd79
两仪逆 <81500039+3151568897@users.noreply.github.com>
2024-08-13 10:22:29 +0800

    gateway启动!!!

---
+++ myMall-gateway/src/test/java/com/example/mymall/gateway/MyMallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
